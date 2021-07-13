ab9f134f09f43e24bdad5bea3c225062df7406ad
esterbvila <ester.batllori@gmail.com>
2021-07-13 13:54:21 +0200

    total increase and decrease

--- script.js
+++ script.js
@@ -1,27 +1,75 @@
 //· 1 ITEM CLICKED:
-//* 1.1 CHANGE BG COLOR & 1.2 ITEM STATUS & 1.3 MOVE ITEM UNDER LIST
-
 var shoppingList = document.querySelector('ol');
-shoppingList.addEventListener('click', function(ev) {
+
+shoppingList.addEventListener('click', function changeStatus(ev) {
   if (ev.target.tagName === 'LI') {
+
+    //* 1.1 BG COLOR CHANGES
     ev.target.classList.toggle('checked');
 
+    //* 1.2 ITEM STATUS ICON CHANGES
     var checked = ev.target.firstElementChild.innerHTML = '<i class="fas fa-check-circle"></i>';
     checked
 
-    document.querySelector('ol').appendChild(ev.target)
+    //* 1.3 MOVE ITEM UNDER LIST
+    setTimeout(function(){document.querySelector('ol').appendChild(ev.target)},1000);
+
+    //* 1.4 TOTAL INCREASES
+    var getLastChild = ev.target.lastElementChild;
+
+    var getGrandChild = getLastChild.lastElementChild.innerHTML
+
+    var price = parseFloat(getGrandChild)
+
+    var totalSenseEur = parseFloat(document.getElementById('totalPriceNum').innerHTML)
+
+    var total = totalSenseEur +price;
+
+    console.log(total)
+
+    document.getElementById('totalPriceNum').innerHTML = Math.round((total + Number.EPSILON) * 100) / 100 + ' EUR'
+
 
         if(ev.target.classList.contains('checked')==false){
             var unChecked = ev.target.firstElementChild.innerHTML = '<i class="far fa-circle"></i>';
             unChecked
+            setTimeout(function(){document.querySelector('ol').prepend(ev.target)},1000);
 
-            document.querySelector('ol').prepend(ev.target)
+            //* 1.4 TOTAL DECREASES
+            var getLastChild = ev.target.lastElementChild;
 
-        }      
-    }
+            var getGrandChild = getLastChild.lastElementChild.innerHTML
+
+            var price = parseFloat(getGrandChild)
+
+            var totalSenseEur = parseFloat(document.getElementById('totalPriceNum').innerHTML)
+
+            var total = totalSenseEur -price - price;
+
+            console.log(total)
+
+            document.getElementById('totalPriceNum').innerHTML = Math.round((total + Number.EPSILON) * 100) / 100 + ' EUR'
+
+        }   
+     }  
+  
 });
 
+/*var children = document.querySelector('ol').children;
+console.log(children);
+for(var i = 0; i<children.length;i++){
+    console.log(children[i])
+
+    document.getElementById(`${children[i].id}`).addEventListener('click', function(ev) {
+        console.log('hola')
+        ev.target.parentElement.classList.toggle('checked');
+        console.log('adeu')
+}*/
+
+
 //* 1.4 TOTAL INCREASES
+
+
 
 //* 1.5 RESET BUTTON APPEARS
 
