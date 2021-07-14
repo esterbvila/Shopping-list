cab6a6e891f04f52571b7d79d510849b8d097591
esterbvila <ester.batllori@gmail.com>
2021-07-14 12:16:55 +0200

    reduced code

--- script.js
+++ script.js
@@ -1,68 +1,45 @@
 //· 1 ITEM CLICKED:
 var shoppingList = document.querySelector('ol');
-
 shoppingList.addEventListener('click', function changeStatus(ev) {
+
+    //* TOTAL INCREASES/DECREASES
+    var getLastChild = ev.target.lastElementChild;
+    var getPriceItem = parseFloat(getLastChild.lastElementChild.innerHTML)
+    var getTotal= document.getElementById('totalPriceNum').innerHTML
+    var parseTotal = parseFloat(getTotal)
+    var setTotal;
+
     if(ev.target.tagName === 'LI') {
-        //* 1.1 BG COLOR CHANGES
+        //* TOTAL INCREASES
+        setTotal = parseTotal +getPriceItem;
+        document.getElementById('totalPriceNum').innerHTML = Math.round((setTotal + Number.EPSILON) * 100) / 100 + ' EUR'
+        //* BG COLOR CHANGES
         ev.target.classList.toggle('checked');
-
-        //* 1.2 ITEM STATUS ICON CHANGES
-        var checked = ev.target.firstElementChild.innerHTML = '<i class="fas fa-check-circle"></i>';
-        checked
-
-        //* 1.3 MOVE ITEM UNDER LIST
+        //* ITEM STATUS ICON CHANGES
+        ev.target.firstElementChild.innerHTML = '<i class="fas fa-check-circle"></i>';
+        //* MOVE ITEM UNDER LIST
         setTimeout(function(){document.querySelector('ol').appendChild(ev.target)},500);
 
-        //* 1.4 TOTAL INCREASES
-        var getLastChild = ev.target.lastElementChild;
-
-        var getLastGrandChild = getLastChild.lastElementChild.innerHTML
-
-        var getNumOfLGC = parseFloat(getLastGrandChild)
-
-        var getTotalOfList = parseFloat(document.getElementById('totalPriceNum').innerHTML)
-
-        var updateTotalOfList = getTotalOfList +getNumOfLGC;
-
-        console.log(updateTotalOfList)
-
-        document.getElementById('totalPriceNum').innerHTML = Math.round((updateTotalOfList + Number.EPSILON) * 100) / 100 + ' EUR'
-
     if(ev.target.classList.contains('checked')==false){
-        var unChecked = ev.target.firstElementChild.innerHTML = '<i class="far fa-circle"></i>';
-        unChecked
-
+        //* TOTAL DECREASES
+        setTotal = parseTotal -getPriceItem;
+        document.getElementById('totalPriceNum').innerHTML = Math.round((setTotal + Number.EPSILON) * 100) / 100 + ' EUR'
+        //* ITEM STATUS ICON CHANGES
+        ev.target.firstElementChild.innerHTML = '<i class="far fa-circle"></i>';
+        //* MOVE ITEM UNDER LIST
         setTimeout(function(){document.querySelector('ol').prepend(ev.target)},500);
 
-        //* 1.4 TOTAL DECREASE
-        var getLastChild = ev.target.lastElementChild;
-
-        var getLastGrandChild = getLastChild.lastElementChild.innerHTML
-
-        var getNumOfLGC = parseFloat(getLastGrandChild)
-
-        var getTotalOfList = parseFloat(document.getElementById('totalPriceNum').innerHTML)
-
-        var updateTotalOfList = getTotalOfList -getNumOfLGC -getNumOfLGC;
-
-        console.log(updateTotalOfList)
-
-        document.getElementById('totalPriceNum').innerHTML = Math.round((updateTotalOfList + Number.EPSILON) * 100) / 100 + ' EUR'
-
-    //* 1.5 RESET BUTTON DIS/APPEARS
-    }if(updateTotalOfList == 0){
+    //* RESET BUTTON DIS/APPEARS
+    }if(setTotal == 0){
         document.getElementById('resetListButton').style.opacity='0';
-    }if(updateTotalOfList !== 0){
+    }if(setTotal !== 0){
         document.getElementById('resetListButton').style.opacity='1';
     } 
-}
-
-     
+}     
 });
 
 //· 2 RESET LIST
-var resetListButton = document.getElementById('resetListButton');
-
+var resetListButton = document.getElementById('resetListButton')
 resetListButton.addEventListener('click', function resetList(ev) {
     var liS = document.querySelectorAll('li')
     var liSArr = Array.from(liS);
@@ -75,17 +52,15 @@ resetListButton.addEventListener('click', function resetList(ev) {
 });
 
 //· 3 FILTERED LIST:
-
 function filterShoppingList(){
-    var input, filter, ol, li, i, id, starts;
-    input = document.getElementById('filterBar')
-    filter = input.value.toUpperCase();
-    ol = document.querySelector('ol');
-    li = ol.getElementsByTagName('li');
+    var input = document.getElementById('filterBar')
+    var filter = input.value.toUpperCase();
+    var ol = document.querySelector('ol');
+    var li = ol.getElementsByTagName('li');
 
     for (i = 0; i < li.length; i++) {
-        id = li[i].id.toUpperCase();
-        starts = id.startsWith(filter);
+        var id = li[i].id.toUpperCase();
+        var starts = id.startsWith(filter);
         if(starts){
             li[i].style.display = "";
         }else{
@@ -94,5 +69,4 @@ function filterShoppingList(){
     }
 }
 
-//· 4 CHANGES REMAIN WHEN RELOAD 
-  
+//· 4 CHANGES REMAIN WHEN RELOAD
